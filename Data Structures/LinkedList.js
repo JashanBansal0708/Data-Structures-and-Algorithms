1ae70880fd0689dece2274a2b7a7f6de46c2fd96
jashanbansal0708 <jashanbansal7856@yahoo.com>
2018-12-12 22:53:11 +0530

    Setup some of the linkedlist operations. Some bugs have to remove in functionality

--- Data Structures/LinkedList.js
+++ Data Structures/LinkedList.js
@@ -5,6 +5,14 @@ export default class LinkedList{
         this.head = null
         this.tail = null
     }
+
+    print(){
+        let currentNode = this.head
+        while(currentNode){
+            console.log(currentNode.value + "  ")
+            currentNode = currentNode.next
+        }
+    }
     
     prepend(value){
         const newNode = new LinkedListNode(value, this.head)
@@ -15,8 +23,8 @@ export default class LinkedList{
         return this
     }
 
-    append(value, next){
-        const newNode = new LinkedListNode(value, this.head)
+    append(value){
+        const newNode = new LinkedListNode(value)
         if(!this.head){
             this.head = newNode
             this.tail = newNode
@@ -27,10 +35,100 @@ export default class LinkedList{
         return this
     }
 
-    // deleteHead(){
-    //     if(!this.head){
-    //         return null
-    //     }
-    //     this.head.next.next = 
-    // }
+    deleteOne(value){
+        if(!this.head){
+            return null
+            console.log("No node is exist to delete")
+        }
+
+        let deletedNode = null
+
+        if(this.head.value === value){
+            deletedNode = this.head
+            this.head = this.head.next
+        }
+        let currentNode = this.head
+        while(!currentNode.next){
+            if(currentNode.next.value === value){
+                currentNode.next = currentNode.next.next
+                deletedNode = currentNode
+                return deletedNode
+            }
+            else{
+                currentNode = currentNode.next
+            }
+        }
+        return null
+    }
+
+    deleteAll(value){
+        if(!this.head){
+            return null
+            console.log("No node is exist to delete")
+        }
+        
+        let deletedNode = null
+        while(this.head && this.head.value === value){
+            deletedNode = this.head
+            this.head = this.head.next 
+        }
+        
+        let currentNode = this.head
+        
+        if(currentNode !== null){
+            while(currentNode.next){
+                if(currentNode.next.value === value){
+                    deletedNode = currentNode
+                    currentNode.next = currentNode.next.next
+                } else{
+                    currentNode = currentNode.next
+                }
+            }
+        }
+    }
+
+    deleteHead(){
+        if(!this.head){
+            return null
+            console.log("No node is exist to delete")
+        }
+        const deletedHead = this.head
+
+        if(this.head.next){
+            this.head = this.head.next
+        } else{
+            this.tail = null
+            this.head = null
+        }
+        return deletedHead
+    }
+
+    deleteTail(){
+        if(!this.tail){
+            return null
+            console.log("No node is exist to delete")
+        }
+        
+        let currentNode = this.head
+        
+        let deletedNode = null
+        
+        if(!currentNode.next){
+            deletedNode = this.head
+            this.head = null
+            this.tail = null
+            console.log(deletedNode.value)
+            return deletedNode
+        }
+        let previousNode = this.head
+        currentNode = currentNode.next
+        while(currentNode.next){
+            previousNode = currentNode
+            currentNode = currentNode.next 
+        }
+        deletedNode = currentNode
+        previousNode.next = null 
+        console.log(deletedNode.value)
+        return deletedNode
+    } 
 }
